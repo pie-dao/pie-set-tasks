9889e93a97c3a893537d891662a1e451eaa7c612
Mick de Graaf <mick@mickdegraaf.nl>
2020-06-11 13:14:09 +0200

    config fixes

--- config/kovan.ts
+++ config/kovan.ts
@@ -6,7 +6,7 @@ const config = {
     rebalanceAuctionModuleAddress: '0xeA510E982c92620A19475F8Dc777bAaa3c2A00F5',
     rebalancingSetExchangeIssuanceModule: '0xC2eF8799315E08f4ee08eA29913D2e51dba5aB78',
     rebalancingSetIssuanceModule: '0x91E1489D04054Ae552a369504F94E0236909c53c',
-    RebalancingSetTokenV3Factory: '0xdc5B19c7085eBEE3AF84cf30418c0ECa11Ed1933',
+    rebalancingSetTokenFactoryAddress: '0xdc5B19c7085eBEE3AF84cf30418c0ECa11Ed1933',
     setTokenFactoryAddress: '0x952F78C33D3fb884C00b22e69B9119cd70582F80',
     transferProxyAddress: '0x61d264865756751392C0f00357Cc26ea70D98E3B',
     vaultAddress: '0x45Ab785b6c04f11b5e49B03d60f3642A8Ffe9246',
