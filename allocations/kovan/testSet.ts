be6aa5c2af8bd083b2e859dbb43d23c0c1d3f6b0
Mick de Graaf <mick@mickdegraaf.nl>
2020-06-11 13:14:40 +0200

    Changed allocation

--- allocations/kovan/testSet.ts
+++ allocations/kovan/testSet.ts
@@ -8,14 +8,14 @@ export default {
             name: "WBTC",
             decimals:  8,
             address: "0x595f8DaB94b9c718cbf5c693cD539Fd00b286D3d",
-            weight: new BigNumber(0.77),
+            weight: new BigNumber(0.50),
             value: new BigNumber(9816.63),
         },
         {
             name: "USDC",
             decimals: 6,
             address: "0x15758350DECEA0E5A96cFe9024e3f352d039905a",
-            weight: new BigNumber(0.23),
+            weight: new BigNumber(0.50),
             value: new BigNumber(1),
         }
     ]
