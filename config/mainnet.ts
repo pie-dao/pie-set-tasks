78c16441046f2b0b628575a16f9ef12147def100
Mick de Graaf <mick@mickdegraaf.nl>
2020-06-11 12:22:17 +0200

    Update configs to v3 addresses

--- config/mainnet.ts
+++ config/mainnet.ts
@@ -2,16 +2,16 @@ const config = {
     coreAddress: '0xf55186CC537E7067EA616F2aaE007b4427a120C8',
     exchangeIssuanceModuleAddress: '0x73dF03B5436C84Cf9d5A758fb756928DCEAf19d7',
     kyberNetworkWrapperAddress: '0x9B3Eb3B22DC2C29e878d7766276a86A8395fB56d',
-    protocolViewerAddress: '0x589d4b4d311EFaAc93f0032238BecD6f4D397b0f',
+    protocolViewerAddress: '0x32964bBfC4DC8b4D2CDdB834504d54191d8A89fC',
     rebalanceAuctionModuleAddress: '0xe23FB31dD2edacEbF7d92720358bB92445F47fDB',
     rebalancingSetExchangeIssuanceModule: '0xd4240987D6F92B06c8B5068B1E4006A97c47392b',
     rebalancingSetIssuanceModule: '0xcEDA8318522D348f1d1aca48B24629b8FbF09020',
-    rebalancingSetTokenFactoryAddress: '0x15518Cdd49d83471e9f85cdCFBD72c8e2a78dDE2',
+    rebalancingSetTokenFactoryAddress: '0xA367A2513Cbd5be1c75a745914521a93E011549c',
     setTokenFactoryAddress: '0xE1Cd722575801fE92EEef2CA23396557F7E3B967',
     transferProxyAddress: '0x882d80D3a191859d64477eb78Cca46599307ec1C',
     vaultAddress: '0x5B67871C3a857dE81A1ca0f9F7945e5670D986Dc',
     wrappedEtherAddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    linearAuctionPriceCurve: '0xc7124963Ab16C33E5bF421D4c0090116622B3074'
+    linearAuctionPriceCurve: '0x2048b012c6688996A25bCD9742e7dA1ff272b957'
   };
 
   export default config;
